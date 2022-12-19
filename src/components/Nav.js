e8465f36626a7e39821034dab09bb0e4eb2dbca8
kkh061101 <kkh061101@naver.com>
2022-12-19 18:23:43 +0900

    ㅁㅇㄴㄹ

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,21 @@ import './Nav.scss';
 
 function Nav() {
   return <nav className="nav">
-    asdf
+    <div>
+      <span className="YAML">YAML</span>
+      <div>
+        <span>홈</span>
+      </div>
+      <div>
+        <span>전공</span>
+      </div>
+      <div>
+        <span>인문인성</span>
+      </div>
+      <div>
+        <span>외국어</span>
+      </div>
+    </div>
   </nav>;
 }
 
