6f9cc889e5a4a527809e0806f11ce2d3d01b4bf2
googoo81 <shw9052@naver.com>
2022-12-19 15:58:34 +0900

    인트로 페이지 퍼블리싱

--- src/components/intropage.js
+++ src/components/intropage.js
@@ -1,10 +1,9 @@
 export default function Iner () {
     return  <div className="intro-box">
     <div>
-      <span>YAML</span> <br />
+      <span>YAML</span> <br/>
       <button>
-        <h>GAuth</h>
-        계정으로 로그인하기
+        <h>GAuth</h> <h>계정으로 로그인하기</h>
       </button>
     </div>
   </div>
