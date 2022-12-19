57a780170cdb8c1b3ef09f76c8bba985d7234d7b
googoo81 <shw9052@naver.com>
2022-12-19 15:44:55 +0900

    인트로 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Intropage from './components/intropage'
+
 function App() {
   return (
     <div>
-      <div className="intro-box">
-        
-      </div>
+      <Intropage />
     </div>
   );
 }
--- src/components/intropage.js
+++ src/components/intropage.js
@@ -1,3 +1,11 @@
 export default function Iner () {
-    return 
+    return  <div className="intro-box">
+    <div>
+      <span>YAML</span> <br />
+      <button>
+        <h>GAuth</h>
+        계정으로 로그인하기
+      </button>
+    </div>
+  </div>
 }
