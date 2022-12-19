a49248c294727da33ed2bca54bd07b7d56390982
kkh061101 <kkh061101@naver.com>
2022-12-19 20:24:34 +0900

    ㅁㄴㅇㄹ

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,19 +6,28 @@ function Nav() {
     <div>
       <span className="YAML">YAML</span>
       <div>
-        <img height={'20px'} src="https://cdn.discordapp.com/attachments/1054184662228025354/1054351295475097640/Vector-3.png" />
+        <img src="https://cdn.discordapp.com/attachments/1054184662228025354/1054351295475097640/Vector-3.png" alt="home" />
         <div>
           <span>홈</span>
         </div>
       </div>
       <div>
-        <span>전공</span>
+        <img src="https://cdn.discordapp.com/attachments/1054184662228025354/1054351295902924871/Vector-2.png" alt="전공" />
+        <div>
+          <span>전공</span>
+        </div>
       </div>
       <div>
-        <span>인문인성</span>
+        <img src="https://cdn.discordapp.com/attachments/1054184662228025354/1054351295072452668/Vector.png" alt="인문인성" />
+        <div>
+          <span>인문인성</span>
+        </div>
       </div>
       <div>
-        <span>외국어</span>
+        <img src="https://cdn.discordapp.com/attachments/1054184662228025354/1054351296288792607/Vector-1.png" alt="외국어" />
+        <div>
+          <span>외국어</span>
+        </div>
       </div>
     </div>
   </nav>;
