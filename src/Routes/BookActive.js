3115eb3ccf0a263880b514fd59b41b3bc5c5badc
kkh061101 <kkh061101@naver.com>
2022-12-22 08:43:23 +0900

    ㅁㄴㅇㄹ

--- src/Routes/BookActive.js
+++ src/Routes/BookActive.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function BookActive() {
   return <div className="BookActive">
-    BookActive
+    <div className="outsideDiv">
+      <div className="innerDiv">
+        <div className="header">
+          <p>독서활동</p>
+          <button>총점수</button>
+        </div>
+      </div>
+      <label>숫자</label>
+    </div>
   </div>;
 }
 
