a135ade191d79ca67f8657fbc89db3ac108aed77
kkh061101 <kkh061101@naver.com>
2022-12-22 15:32:08 +0900

    체크박스 이미지 수정

--- src/Routes/BookActiveContent.js
+++ src/Routes/BookActiveContent.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BookActiveContent(props) {
+  const [checked, setChecked] = useState(false);
   return <div className="BookActiveContent">
-    <input type='checkbox' />
-    <span>asd</span>
+    {/* <input type='checkbox' onClick={(e) => { setChecked(e.target.checked); }} /> */}
+    {checked ? <div onClick={() => setChecked(() => !checked)} className='checkbox_on' /> : <div onClick={() => setChecked(() => !checked)} className='checkbox' />}
+    <label>{checked ? 'true' : 'false'}</label>
+    {checked && <div>
+      <button>상세보기</button>
+      <button>삭제</button>
+    </div>}
   </div>;
 }
 
