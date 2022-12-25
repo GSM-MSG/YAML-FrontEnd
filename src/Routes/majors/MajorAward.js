5909286ed2eb90778fce46a5993a5d933f5789bc
kkh061101 <kkh061101@naver.com>
2022-12-25 22:14:36 +0900

    box-shadow 적용 불가 현상 해결

--- src/Routes/majors/MajorAward.js
+++ src/Routes/majors/MajorAward.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function MajorAward() {
   return <div className="MajorAward">
-    majoraward
+    <div className="outsideDiv">
+      <div className="innerDiv">
+        <div className="header">
+          <p>수상경력</p>
+          <div className="score">100점</div>
+        </div>
+        <div className="contentBoxes">
+          asdf
+        </div>
+      </div>
+    </div>
   </div>;
 }
 
